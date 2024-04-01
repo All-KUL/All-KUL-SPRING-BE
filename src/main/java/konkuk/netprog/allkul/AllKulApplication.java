6d6ff41472ac0bd74afcd45a153daf22f7e63910
judemin <judemin2087@naver.com>
2024-04-01 23:24:08 +0900

    ADD : Add DataSource Exclude Option

--- src/main/java/konkuk/netprog/allkul/AllKulApplication.java
+++ src/main/java/konkuk/netprog/allkul/AllKulApplication.java
@@ -2,8 +2,10 @@ package konkuk.netprog.allkul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// DataSource Exclude 옵션 추가
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class AllKulApplication {
 
 	public static void main(String[] args) {
