b82910ccb5c6b8e17d358d058a77581fcfad6443
judemin <judemin2087@naver.com>
2024-04-01 22:22:29 +0900

    INIT : Initialized Project

---
+++ src/main/java/konkuk/netprog/allkul/AllKulApplication.java
@@ -0,0 +1,13 @@
+package konkuk.netprog.allkul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllKulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllKulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konkuk/netprog/allkul/AllKulApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.netprog.allkul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllKulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
